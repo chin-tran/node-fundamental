54716a526bff2eaf86f63402c50869fc0f5bd9e2
chin-tran <chinbkit09@gmail.com>
2022-11-07 15:16:00 +0700

    Add example pipe

--- stream.js
+++ stream.js
@@ -1,9 +1,15 @@
 var fs = require('fs');
+var zlib = require('zlib');
 
 var readable = fs.createReadStream(__dirname + '/stream_data.txt', { encoding: 'utf8', highWaterMark: 16 * 1024});
 var writeable = fs.createWriteStream(__dirname + '/stream_data_copy.txt');
+var compressWriteable = fs.createWriteStream(__dirname + '/stream_data.gz');
+var gzip = zlib.createGzip();
 
-readable.on('data', (chunk) => {
-    console.log(chunk.length + ' bytes');
-    // writeable.write(chunk);
-})
+// readable.on('data', (chunk) => {
+//     console.log(chunk.length + ' bytes');
+//     // writeable.write(chunk);
+// })
+
+readable.pipe(writeable);
+readable.pipe(gzip).pipe(compressWriteable);
